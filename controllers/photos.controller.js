07bbe1be3350a068a8441a84d4fc22e1da93d79c
Pawel Wisniewski <pawel_wisniewski@outlook.com>
2021-06-16 13:30:50 +0200

    Add input patterns validation

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -12,6 +12,15 @@ exports.add = async (req, res) => {
 
       const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
       const fileExt = fileName.split('.').slice(-1)[0];
+
+      const titlePattern = new RegExp(/^([A-z]|\s|[0-9]|\.){1,25}$/);
+      if (!titlePattern.test(title)) throw new Error('Invalid title pattern...');
+
+      const authorPattern = new RegExp(/^([A-z]|\s|[0-9]|\.){1,50}$/);
+      if (!authorPattern.test(author)) throw new Error('Invalid author pattern...');
+
+      const emailPattern = new RegExp(/^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.([a-z]{1,6}))$/i);
+      if (!emailPattern.test(email)) throw new Error('Invalid email pattern...');
       
       if (['jpg', 'png'].includes(fileExt) && title.length <= 25 && author.length <= 50) {
         const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
